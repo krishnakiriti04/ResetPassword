d41af42b1a229eb35cdba0ecfe69dc0b06749392
krishna kireeti <krishnakiriti.mamidi@gmail.com>
2020-11-07 00:04:37 +0530

    third commit

--- index.js
+++ index.js
@@ -10,13 +10,18 @@ const client = mongodb.MongoClient;
 
 
 const app = express();
-const dburl =  process.env.DB_URL ||"mongodb://localhost:27017";
+const dburl = process.env.DB_URL || "mongodb://localhost:27017";
 const port = process.env.PORT || 4000;
 
 //middle ware
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(cors());
 app.use(bodyparser.json())
+
+app.get('/', async(req, res) => {
+    res.send("Welcome to main page!!!")
+})
+
 
 //register
 app.post('/register', async(req, res) => {
